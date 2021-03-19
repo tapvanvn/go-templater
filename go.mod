277ab02ee098c785ecb4690da55749792c70df42
duy.nguyen <tapvanvn@gmail.com>
2021-03-19 13:17:35 +0700

    update

--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module github.com/tapvanvn/gotemplater
 go 1.16
 
 require (
-	github.com/tapvanvn/gotokenize v0.0.1
+	github.com/tapvanvn/gotokenize v1.0.3
 )
--- templater.go
+++ templater.go
@@ -1,21 +1,26 @@
 package gotemplater
 
+import (
+	"fmt"
+)
+
 //Templater manage
 type templater struct {
 	namespaces map[string]string //namespace to path
 }
 
-func (tpt *templater) Debug(){
+func (tpt *templater) Debug() {
 
 	fmt.Println("debug")
 }
+
 var Templater *templater = nil
 
 //InitTemplater should call once at begining of app to init templater
 func InitTemplater() *templater {
 	if Templater == nil {
 		Templater = &templater{
-			namespaces: map[string]string{}
+			namespaces: map[string]string{},
 		}
 	}
 	return Templater
